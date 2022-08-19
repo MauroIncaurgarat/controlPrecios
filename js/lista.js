f3b2d3e2a43fc28e274c70105e2b54d42f0ddc57
unknown <mauroincaurgarat@gmail.com>
2022-08-18 22:49:47 -0300

    Cargar datos del local Storage

--- js/lista.js
+++ js/lista.js
@@ -10,7 +10,7 @@ let ProductName;
 let ProductPresentation;
 let ProductCost;
 let ProductUtility;
-let NombreProveedor = prompt("Ingrese el nombre del Proveedor"); //Clave de Productos
+
 //FUNCIONAMIENTO - FLOW
 form.addEventListener("submit", function(event) {
     //cancelo el envio al servidor
@@ -19,12 +19,27 @@ form.addEventListener("submit", function(event) {
     ProductFormData = new FormData(form); 
     //convertir datos a objeto
     ProductObj = convertProductFormDataToProductObj(ProductFormData); 
-    //guardar en localStorage
-    saveProductObj(ProductObj); 
     //insertar datos a la tabla
     insertRowInTable(ProductObj);
+    //guardar en localStorage
+    saveProductObj(ProductObj); 
 });
+//Cargar lo guardado en el localStorage
+    document.addEventListener('DOMContentLoaded', (event) => { 
+        //trigo la informacion del sessionStorage, la transformo a objeto y la guardo en productObjArr
+        let productObjArr = JSON.parse(localStorage.getItem("Producto")) 
+        
+        //Recorro los elementos del Array y le aplico una funcion
+        productObjArr.forEach(
+            //la funcion guarda los elementos del array en "arrayElement" y los inserta en la tabla
+            function(arrayElement){
+                insertRowInTable(arrayElement)
+            }
+        )
+    }); 
 
+
+                                //FUNCTIONS
 //funciones de Calculo
 function VentaNeta (x,y){
     return x*((y/100)+1);  //costo por utilidad (utilidad - transformar 50 en 1.5)
@@ -36,12 +51,17 @@ function Iva(x){
 function convertProductFormDataToProductObj (ProductFormData){
     //almaceno los valores obtenidos del formulario en variables
     ProductPresentation = ProductFormData.get("ProductPresentation");
+
     ProductName = ProductFormData.get("ProductName");
+
     ProductCost = ProductFormData.get("ProductCost");
+    
     ProductUtility = ProductFormData.get("ProductUtility");
+    
     //aplico funciones para los datos que me faltan
     ProductNetSale = VentaNeta(ProductFormData.get("ProductCost"),ProductFormData.get("ProductUtility"));
     ProductFinalSale = Iva(ProductNetSale);
+    
     return{ //retorno un OBJETO
         "ProductName": ProductName,
         "ProductCost": ProductCost,
@@ -80,19 +100,26 @@ function insertRowInTable(ProductObj){
     newProductRowCell = newProductRowRef.insertCell(6);
     newProductRowCell.textContent = ProductObj["ProductFinalSale"];
 }
-
 // Funcion Almacenamiento en el localStorage
-
 function saveProductObj (ProductObj){   
     /*Funcionamiento: Mi programa lee lo que se encuentra almcaenado en el localStorage y el añade un nuevo objeto, en el caso de que el usuario vaya iterando en el formulario */  
     
-    /*Guardo en mi array los objetos que pueden estar almacenados en mi localStorage. Tomo los datos y los transformo. En el caso que esté vacio, tengo que declararle un array vacio para evitar que me devuelva null y se rompa mi programa*/ 
+    //si mi localStorage está vacio --> que guarde un array vacio (evito guardar "null" y que se rompa el codigo)
     let productArray = JSON.parse(localStorage.getItem("Producto")) || [];
+    
     //ingreso los nuevos objetos al array
     productArray.push(ProductObj);
+    
     // Paso my array a JSON para poder almacenarlo
     let productArrayJSON = JSON.stringify(productArray); //transformo el objeto a string
+    
     // guardo en el localStorage
-    localStorage.setItem(NombreProveedor, productArrayJSON);
-
+    localStorage.setItem("Producto", productArrayJSON);
+    
+    /* En el futuro quier que:
+    - La llave del array sea el nombre del proveedor
+    - Crear una opcion de guardar, para almacenar proveedores distintos
+    
+    */
 }
+
